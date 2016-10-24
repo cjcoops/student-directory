f80c5a8c05397f6b754e74cd0302cc273a675c0e
cjcoops <cjcooper252@gmail.com>
2016-10-24 14:37:46 +0100

    prints names less than 12

--- directory.rb
+++ directory.rb
@@ -29,7 +29,7 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
+  students.select{|student| student[:name].length < 13}.each_with_index do |student, index|
     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
