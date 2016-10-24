d33749f6b8ca73b00fdcff7c4e38ac0f9ebafee3
cjcoops <cjcooper252@gmail.com>
2016-10-24 19:29:31 +0100

    added save students

--- directory.rb
+++ directory.rb
@@ -19,6 +19,18 @@ def input_students
   end
 end
 
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 #def begin_with(students)
 #  puts "Enter the first letter of the students name you wish to view"
 #  choice = gets.chomp.downcase
@@ -69,6 +81,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -84,6 +97,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
